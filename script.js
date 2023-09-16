b1d736dbdc4f93d79f689838d2df8e4d1fc54e52
Vishvishwanathan <vishvishva204@gmail.com>
2023-09-16 14:50:38 +0530

    Script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function foo(){
+var res=document.getElementById(Firstname).value
+console.log(res)
+var res=document.getElementById(Middlename ).value
+console.log(res)
+var res=document.getElementById(lastname).value
+console.log(res)
+var res=document.getElementById(Email).value
+console.log(res)
+var res=document.getElementById(Pincode).value
+console.log(res)
+
+
+}
